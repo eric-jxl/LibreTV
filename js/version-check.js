99d73eb7d74f1a6a33f365551c4e13f8d36f45e6
Eric <799050481@qq.com>
2025-06-18 14:00:26 +0800

    修改图标及打包

--- js/version-check.js
+++ js/version-check.js
@@ -37,7 +37,7 @@ async function checkForUpdates() {
         // 获取最新版本
         let latestVersion;
         const VERSION_URL = {
-            PROXY: 'https://raw.ihtw.moe/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
+            PROXY: 'https://gh-proxy.com/raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt',
             DIRECT: 'https://raw.githubusercontent.com/LibreSpark/LibreTV/main/VERSION.txt'
         };
         const FETCH_TIMEOUT = 1500;
